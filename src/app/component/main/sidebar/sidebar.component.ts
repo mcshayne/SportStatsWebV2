2d4e0db219ae97d0bd0db7b3c900a280c9ff21d9
Jessica Astorsson <koppargodis@outlook.com>
2022-10-21 17:47:45 +0200

    Simple boolean based switch for dropdown visibility

--- src/app/component/main/sidebar/sidebar.component.ts
+++ src/app/component/main/sidebar/sidebar.component.ts
@@ -1,15 +1,26 @@
+import { BinaryOperatorExpr } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  protected dropdownContainerIsVisible: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  switchDropdownContainerVisibility(){
+    if(this.dropdownContainerIsVisible) {
+      this.dropdownContainerIsVisible = false;
+    }
+    else {
+      this.dropdownContainerIsVisible = true;
+    }
   }
 
 }
