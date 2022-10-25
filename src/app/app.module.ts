f7452e784305794ecde147a0bb9a3db2f70c0a4c
Jessica Astorsson <koppargodis@outlook.com>
2022-10-25 21:20:37 +0200

    Mergin with develop and adding the required imports etc to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatTableModule} from '@angular/material/table'; 
+import { MatTableModule } from '@angular/material/table'; 
 import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,9 +45,16 @@ import { LeagueComponent } from './component/league/league.component';
     AppRoutingModule,
     MatTableModule,
     HttpClientModule,
-    MatCardModule
-  ],
-  providers: [],
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    NoopAnimationsModule,
+    BrowserAnimationsModule
+],
+providers: [
+  {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}} // Used by contact.component
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
