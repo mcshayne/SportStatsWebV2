644ecdfb0f1b053c317f42021aafff75c73aac25
David <davidsjoblom@hotmail.se>
2022-10-26 12:18:49 +0200

    Made a sport component, so all other components might be able to be removed

--- src/app/component/league/league.component.ts
+++ src/app/component/league/league.component.ts
@@ -16,79 +16,85 @@ export class LeagueComponent {
   @Input() sportIdentifier: string = "";
   @Output() getLeagueId = new EventEmitter<number>();
 
-  addNewItem(value:number) {
+  addNewItem(value: number) {
     this.getLeagueId.emit(value);
-    console.log('clicked ' + value) 
+    console.log('clicked ' + value)
   }
-  
+
   constructor(private leagueService: LeagueService) { }
- 
+
   ngOnInit(): void {
-      if(this.sportIdentifier.match("floorball")) {
-        this.getDataFlM();
-        this.getDataFlW();
-      }
-      if(this.sportIdentifier.match("football")) {
-        this.getDataFoM();
-        this.getDataFoW();
-      }
-      if(this.sportIdentifier.match("hockey")) {
-        this.getDataIceM();
-        this.getDataIceW();
-      }
+    if (this.sportIdentifier.match("Floorball")) {
+      this.getDataFlM();
+      this.getDataFlW();
+    }
+    if (this.sportIdentifier.match("Football")) {
+      this.getDataFoM();
+      this.getDataFoW();
+    }
+    if (this.sportIdentifier.match("Hockey")) {
+      this.getDataIceM();
+      this.getDataIceW();
+    }
   }
-  public getDataFoM():void{
-    this.leagueService.getFootballLeaguesM().subscribe(res => 
-      {this.leaguesM = Object.values(res)[1]
-        const key = 'name';
+  public getDataFoM(): void {
+    this.leagueService.getFootballLeaguesM().subscribe(res => {
+      this.leaguesM = Object.values(res)[1]
+      const key = 'name';
       let tempName = new Set(new Map(this.leaguesM.map((item: any) => [item[key], item.id])))
       this.leaguesM = tempName;
-        console.log(res)})
-    }
-  
-    public getDataFoW():void{
-      this.leagueService.getFootballLeaguesW().subscribe(res => 
-        {this.leaguesW = Object.values(res)[1]
-          const key = 'name';
-          let tempName = new Set(new Map(this.leaguesW.map((item: any) => [item[key], item.id])))
-          this.leaguesW = tempName;
-            console.log(res)})
-      }
-    
-      public getDataFlM():void{
-        this.leagueService.getFloorballLeaguesM().subscribe(res => 
-          {this.leaguesM = Object.values(res)[1]
-            const key = 'name';
-            let tempName = new Set(new Map(this.leaguesM.map((item: any) => [item[key], item.id])))
-            this.leaguesM = tempName;
-              console.log(res)})
-        }
-      
-        public getDataFlW():void{
-          this.leagueService.getFloorballLeaguesW().subscribe(res => 
-            {this.leaguesW = Object.values(res)[1]
-              const key = 'name';
-              let tempName = new Set(new Map(this.leaguesW.map((item: any) => [item[key], item.id])))
-              this.leaguesW = tempName;
-                console.log(res)})
-          }
+      console.log(res)
+    })
+  }
 
-          public getDataIceM():void{
-            this.leagueService.getHockeyLeaguesM().subscribe(res => 
-              {this.leaguesM = Object.values(res)[1]
-                const key = 'name';
-                let tempName = new Set(new Map(this.leaguesM.map((item: any) => [item[key], item.id])))
-                this.leaguesM = tempName;
-                  console.log(res)})
-            }
+  public getDataFoW(): void {
+    this.leagueService.getFootballLeaguesW().subscribe(res => {
+      this.leaguesW = Object.values(res)[1]
+      const key = 'name';
+      let tempName = new Set(new Map(this.leaguesW.map((item: any) => [item[key], item.id])))
+      this.leaguesW = tempName;
+      console.log(res)
+    })
+  }
 
-            public getDataIceW():void{
-              this.leagueService.getHockeyLeaguesW().subscribe(res => 
-                {this.leaguesW = Object.values(res)[1]
-                  const key = 'name';
-                  let tempName = new Set(new Map(this.leaguesW.map((item: any) => [item[key], item.id])))
-                  this.leaguesW = tempName;
-                    console.log(res)})
-              }
-  
+  public getDataFlM(): void {
+    this.leagueService.getFloorballLeaguesM().subscribe(res => {
+      this.leaguesM = Object.values(res)[1]
+      const key = 'name';
+      let tempName = new Set(new Map(this.leaguesM.map((item: any) => [item[key], item.id])))
+      this.leaguesM = tempName;
+      console.log(res)
+    })
+  }
+
+  public getDataFlW(): void {
+    this.leagueService.getFloorballLeaguesW().subscribe(res => {
+      this.leaguesW = Object.values(res)[1]
+      const key = 'name';
+      let tempName = new Set(new Map(this.leaguesW.map((item: any) => [item[key], item.id])))
+      this.leaguesW = tempName;
+      console.log(res)
+    })
+  }
+
+  public getDataIceM(): void {
+    this.leagueService.getHockeyLeaguesM().subscribe(res => {
+      this.leaguesM = Object.values(res)[1]
+      const key = 'name';
+      let tempName = new Set(new Map(this.leaguesM.map((item: any) => [item[key], item.id])))
+      this.leaguesM = tempName;
+      console.log(res)
+    })
+  }
+
+  public getDataIceW(): void {
+    this.leagueService.getHockeyLeaguesW().subscribe(res => {
+      this.leaguesW = Object.values(res)[1]
+      const key = 'name';
+      let tempName = new Set(new Map(this.leaguesW.map((item: any) => [item[key], item.id])))
+      this.leaguesW = tempName;
+      console.log(res)
+    })
+  }
+
 }
--- src/app/component/sport/sport.component.ts
+++ src/app/component/sport/sport.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sport',
@@ -11,19 +11,35 @@ export class SportComponent implements OnInit  {
   leagueId: number = 0;
   tableDiv: boolean = false;
   leagueDiv: boolean = true;
-  sportIdentifier: string = "";
+  sportIdentifier: string ="" ;
   sub: any;
+  someSubscription: any;
 
-
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,private router:Router) {
+    /* Code that makes the page refreash once you press a new key in the nav-bar*/
+    this.router.routeReuseStrategy.shouldReuseRoute = function () {
+    return false;
+  };
+  this.someSubscription = this.router.events.subscribe((event) => {
+    if (event instanceof NavigationEnd) {
+      // Here is the dashing line comes in the picture.
+      // You need to tell the router that, you didn't visit or load the page previously, so mark the navigated flag to false as below.
+      this.router.navigated = false;
+    }
+  });}
 
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.sportIdentifier = params['sportName'];
+    this.route.paramMap.subscribe(params => {
+      this.sportIdentifier = params.get("sportName") as string;
       
     });
     console.log(this.sportIdentifier)
+  }
+  ngOnDestroy() {
+    if (this.someSubscription) {
+      this.someSubscription.unsubscribe();
+    }
   }
 
   public switchView(id:number) {
