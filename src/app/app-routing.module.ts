7c0b1e3f13a9961d50d845973301cafc1418c314
David <davidsjoblom@hotmail.se>
2022-10-26 12:44:01 +0200

    Removed hockey/football/floorball component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './component/contact/contact.component';
-import { FloorballComponent } from './component/floorball/floorball.component';
-import { FootballComponent } from './component/football/football.component';
-import { HockeyComponent } from './component/hockey/hockey.component';
 import { HomeComponent } from './component/home/home.component';
 import { MainComponent } from './component/main/main.component';
 import { SportComponent } from './component/sport/sport.component';
@@ -12,9 +9,6 @@ const routes: Routes = [
   {path:'', component: MainComponent},
   {path:'sport/:sportName',component:SportComponent},
   {path:'home', component: HomeComponent},
-  {path:'football', component: FootballComponent},
-  {path:'hockey', component: HockeyComponent},
-  {path:'floorball', component: FloorballComponent},
   {path:'contact', component: ContactComponent}
 ];
 
@@ -23,3 +17,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,6 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './component/main/main.component';
 import { HomeComponent } from './component/home/home.component';
 import { ContactComponent } from './component/contact/contact.component';
-import { FootballComponent } from './component/football/football.component';
-import { HockeyComponent } from './component/hockey/hockey.component';
-import { FloorballComponent } from './component/floorball/floorball.component';
 import { NavbarComponent } from './component/main/navbar/navbar.component';
 import { SidebarComponent } from './component/main/sidebar/sidebar.component';
 import { TableComponent } from './component/table/table.component';
@@ -33,9 +30,6 @@ import { SportComponent } from './component/sport/sport.component';
     MainComponent,
     HomeComponent,
     ContactComponent,
-    FootballComponent,
-    HockeyComponent,
-    FloorballComponent,
     NavbarComponent,
     SidebarComponent,
     TableComponent,
